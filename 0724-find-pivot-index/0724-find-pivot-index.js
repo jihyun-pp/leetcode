2e2e1dde5dd076db85806278efa9487649738014
JIHYUN <qkrwlw1211@gmail.com>
2022-11-13 00:19:58 +0900

    Time: 65 ms (99.46%), Space: 44.2 MB (85.79%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.js
+++ 0724-find-pivot-index/0724-find-pivot-index.js
@@ -1,7 +1,5 @@
-/**
- * @param {number[]} nums
- * @return {number}
- */
+// Find Pivot Index
+// Pivot Index : 해당 인덱스를 기준으로 오른쪽 배열의 값들의 합과 왼쪽 배열의 값들이 합이 같은 경우
 const pivotIndex = function(nums) {
     let total = nums.reduce((a, b) => a + b);
     let leftSum = 0;
