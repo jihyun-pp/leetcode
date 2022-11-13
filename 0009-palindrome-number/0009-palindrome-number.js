1c877a8e1c08063d2013d22f11840d82883dd391
JIHYUN <qkrwlw1211@gmail.com>
2022-11-13 19:38:07 +0900

    Time: 335 ms (32.70%), Space: 51.6 MB (23.47%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.js
@@ -0,0 +1,8 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    var z = String(x).split('').reverse().join('');
+    return parseInt(z) === x ? true : false;
+};
