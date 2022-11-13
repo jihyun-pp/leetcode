d25ffb957b41a05828a4259ce8bb44dc36ab3dca
JIHYUN <qkrwlw1211@gmail.com>
2022-11-13 23:11:10 +0900

    Time: 122 ms (5.14%), Space: 43.6 MB (6.76%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.js
+++ 0035-search-insert-position/0035-search-insert-position.js
@@ -9,11 +9,14 @@ var searchInsert = function(nums, target) {
     if(output === -1){
         nums.push(target);
         // nums.sort();
-        nums.sort(function(a, b) {
-            return a - b;
-        });
-        console.log(nums);
+        nums.sort((a, b) => a - b);
         output = nums.indexOf(target);
     }
     return output;
 };
+
+/** 
+* https://developer.mozilla.org/ko/docs/Web/JavaScript/Reference/Global_Objects/Array/sort
+* 기본 정렬 순서는 문자열의 유니코드 코드 포인트를 따름 >> 오름차순 정렬 안될 수 있음 
+* 숫자 오름차순으로 정렬하고 싶으면 function(a, b)
+*/
