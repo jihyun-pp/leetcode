17765ceb3b7b60ddb2618b6cb5d3f571e5c3668f
JIHYUN <qkrwlw1211@gmail.com>
2022-11-20 20:48:55 +0900

    Time: 127 ms (20.13%), Space: 44 MB (77.82%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
@@ -11,26 +11,14 @@
  * @return {ListNode}
  */
 const mergeTwoLists = function(list1, list2) {
+    if (!list1) return list2;
+    if (!list2) return list1;
     
-    const dumy = new ListNode(0);
-    let current = dumy;
-    
-    while (list1 !== null && list2 !== null) {
-        if (list1.val <= list2.val) {
-            current.next = new ListNode(list1.val);
-            list1 = list1.next;
-        } else {
-            current.next = new ListNode(list2.val);
-            list2 = list2.next;
-        }
-        current = current.next;
-    }
-    
-    if (list1 !== null) {
-        current.next = list1;
+    if (list1.val <= list2.val) {
+        list1.next = mergeTwoLists(list1.next, list2);
+        return list1;
     } else {
-        current.next = list2;
+        list2.next = mergeTwoLists(list1, list2.next);
+        return list2;
     }
-    
-    return dumy.next;   
 };
