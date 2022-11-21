cf683dd590323f968f506de0766580c5a3f32690
JIHYUN <qkrwlw1211@gmail.com>
2022-11-21 22:57:05 +0900

    Time: 84 ms (84.59%), Space: 41.8 MB (74.04%) - LeetHub

---
+++ 1523-count-odd-numbers-in-an-interval-range/1523-count-odd-numbers-in-an-interval-range.js
@@ -0,0 +1,8 @@
+/**
+ * @param {number} low
+ * @param {number} high
+ * @return {number}
+ */
+const countOdds = function(low, high) {
+    return Math.floor((high - low)/2) + (low%2 || high%2 ? 1 : 0);
+};
